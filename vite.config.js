678b16d4cc509091ba7acd80a2721a80205f4a47
Oghenekparobor Uduvwotayinre <alexiskpasv@gmail.com>
2026-03-26 15:08:14 +0100

    built vite for github pages build

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/win11-calc/', // Critical: Must match your GitHub repo name
   server: {
     port: 5173,
     open: true, // Automatically opens the browser when you run npm run dev
